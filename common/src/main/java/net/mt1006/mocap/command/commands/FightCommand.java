9ee297c66c51afbfb007551e2ed0c3f9d1bdc8ac
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-21 01:06:42 +0530

    fix: restore Fight root command closure

--- common/src/main/java/net/mt1006/mocap/command/commands/FightCommand.java
+++ common/src/main/java/net/mt1006/mocap/command/commands/FightCommand.java
@@ -138,7 +138,7 @@ public final class FightCommand
 															.then(Commands.argument("y", DoubleArgumentType.doubleArg())
 																.then(Commands.argument("z", DoubleArgumentType.doubleArg())
 																	.executes(CommandUtils.command(FightCommand::teleportGroup)))))))))
-				))));
+				));
 	}
 
 	private static boolean list(CommandInfo info)
