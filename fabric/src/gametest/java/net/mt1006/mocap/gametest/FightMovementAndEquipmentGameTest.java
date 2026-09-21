a6e48e511de8749f9796b6445e6a8e36a6204771
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-22 01:42:44 +0530

    fix: import game type for crossbow test

--- fabric/src/gametest/java/net/mt1006/mocap/gametest/FightMovementAndEquipmentGameTest.java
+++ fabric/src/gametest/java/net/mt1006/mocap/gametest/FightMovementAndEquipmentGameTest.java
@@ -6,6 +6,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.GameType;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
